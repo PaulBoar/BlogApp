10afd4e60cba107734f0fc724294b52104e12313
PaulBoar <p.dzik1@wp.pl>
2022-06-08 22:39:26 +0200

    setting firebase post&get requests

--- components/comments-section/comments-section.js
+++ components/comments-section/comments-section.js
@@ -10,28 +10,33 @@ function CommentsSection({slug}) {
   setComment(e.target.value)
  }
  const addCommentHandler = () => {
-  setComments(prevComs => [...prevComs, comment])
+  // setComments(prevComs => [...prevComs, comment])
+  postComments({comment: comment})
  }
 
  async function fetchComments() {
-  const res = await fetch(`https://blogapp-c6647-default-rtdb.firebaseio.com/${slug}.json`)
+  const res = await fetch(`https://blogapp-c6647-default-rtdb.firebaseio.com/comments/${slug}.json`)
   const data = await res.json()
   setComments(data)
+  console.log(data)
   }
 
  useEffect( () => {
   fetchComments() 
  }, [])
  
- async function postComments() {
-  const res = await fetch(`https://blogapp-c6647-default-rtdb.firebaseio.com/${slug}.json`, {
+ async function postComments(comment) {
+  const res = await fetch(`https://blogapp-c6647-default-rtdb.firebaseio.com/comments/${slug}.json`, {
    method: 'POST',
-   
+   body: JSON.stringify(comment),
+   headers: {
+    'Content-Type': 'application/json'
+   }
   })
   const data = await res.json()
-  setComments(data)
+  console.log(data)
   }
-
+console.log(comments)
   return (
     <div>
      <form className={styles.form} onSubmit={addCommentHandler}>
@@ -39,7 +44,11 @@ function CommentsSection({slug}) {
       <input type='text' placeholder='Type...' className={styles.input} value={comment} onChange={handleAddComment}/>
       <button type='submit' className={styles.btn}>Add comment</button>
      </form>
-     <div>Forum</div>
+     <div>Forum
+      {/* {comments && comments.map(com => {
+       return <p>{com.comment}</p>
+      })} */}
+     </div>
     </div>
   )
 }
