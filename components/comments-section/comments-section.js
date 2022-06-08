3fc2984cc573e1d445e265e575b100f5042cb35e
PaulBoar <p.dzik1@wp.pl>
2022-06-08 20:09:46 +0200

    comments sec

--- components/comments-section/comments-section.js
+++ components/comments-section/comments-section.js
@@ -1,12 +1,43 @@
+import { useState, useEffect } from 'react'
+
 import styles from './comments-section.module.css'
 
-function CommentsSection() {
+function CommentsSection({slug}) {
+ const [comment, setComment] = useState('')
+ const [comments, setComments] = useState([])
+
+ const handleAddComment = (e) => {
+  setComment(e.target.value)
+ }
+ const addCommentHandler = () => {
+  setComments(prevComs => [...prevComs, comment])
+ }
+
+ async function fetchComments() {
+  const res = await fetch(`https://blogapp-c6647-default-rtdb.firebaseio.com/${slug}.json`)
+  const data = await res.json()
+  setComments(data)
+  }
+
+ useEffect( () => {
+  fetchComments() 
+ }, [])
+ 
+ async function postComments() {
+  const res = await fetch(`https://blogapp-c6647-default-rtdb.firebaseio.com/${slug}.json`, {
+   method: 'POST',
+   
+  })
+  const data = await res.json()
+  setComments(data)
+  }
+
   return (
     <div>
-     <form>
-      <label type='text'>Write a comment</label>
-      <input type='text' placeholder='Type...'/>
-      <button type='submit'>Send</button>
+     <form className={styles.form} onSubmit={addCommentHandler}>
+      <label type='text' className={styles.label}>Write a comment</label>
+      <input type='text' placeholder='Type...' className={styles.input} value={comment} onChange={handleAddComment}/>
+      <button type='submit' className={styles.btn}>Add comment</button>
      </form>
      <div>Forum</div>
     </div>
