a23efc21c63f5236ec9588aba72b0f48ebe9685f
PaulBoar <p.dzik1@wp.pl>
2022-06-13 16:36:55 +0200

    setting firebase auth

--- pages/log-in.js
+++ pages/log-in.js
@@ -1,24 +1,65 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
+import { auth } from '../firebase-config.env.local';
 
-import styles from './log-in.module.css'
+import styles from './log-in.module.css';
 
 function LogIn() {
+  const [wantRegister, setWantRegister] = useState(false);
+  const [registerEmail, setRegisterEmail] = useState('');
+  const [registerPassword, setRegisterPassword] = useState('');
+  const [loginEmail, setLoginEmail] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+console.log(auth)
+  // const register = async () => {
+  //  try {
+
+  //  } catch (error) {
+  //   console.log(error)
+  //  }
+  // };
+
+  const login = async () => {};
+
+  const logout = async () => {};
+
+  const logIn = !wantRegister ? (
+    <>
+      <h2>Log in</h2>
+      <form>
+        <label>Email</label>
+        <input type='text' placeholder='email...' />
+        <label>Password</label>
+        <input type='password' placeholder='password...' />
+        <button type='submit'>Login</button>
+        <button type='button' onClick={() => setWantRegister(true)}>
+          Sign up
+        </button>
+      </form>{' '}
+    </>
+  ) : (
+    <>
+      {' '}
+      <h2>Sign up</h2>
+      <form>
+        <label>Username</label>
+        <input type='text' placeholder='name...' />
+        <label>Email</label>
+        <input type='text' placeholder='email...' />
+        <label>Password</label>
+        <input type='password' placeholder='password...' />
+        <button type='submit'>Sign up</button>
+        <button type='button' onClick={() => setWantRegister(false)}>
+          Log in
+        </button>
+      </form>{' '}
+    </>
+  );
+
   return (
     <div className={styles.container}>
-    <div className={styles.box}>
-     <h2>Log in</h2>
-     <form>
-      <label>Username</label>
-      <input type='text' placeholder='name...'/>
-      <label>Email</label>
-      <input type='text' placeholder='email...'/>
-      <label>Password</label>
-      <input type='password' placeholder='password...'/>
-      <button type='submit'>Log in</button>
-     </form>
+      <div className={styles.box}>{logIn}</div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default LogIn
+export default LogIn;
