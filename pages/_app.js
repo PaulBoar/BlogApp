541b8685be5dde536719f51ee8bc961d7ba49294
PaulBoar <p.dzik1@wp.pl>
2022-06-14 12:58:59 +0200

    sending props from login

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,23 @@
 import '../styles/globals.css';
 import Layout from '../components/layout/layout';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
+ const [isLog, setIsLog] = useState(false)
+ const [name, setName] = useState('')
+const handleIsLogged = (props) => {
+   setIsLog(props)
+   console.log(props)
+}
+
+const handleName = (props) => {
+  setName(props)
+  console.log(props)
+}
+
   return (
-     <Layout>
-      <Component {...pageProps} />
+     <Layout isLogged={isLog} name={name}>
+      <Component {...pageProps} onIsLogged={handleIsLogged} onLoggedName={handleName} />
       </Layout>  
   );
 }
