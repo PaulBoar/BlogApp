682bf2e72c4ab9560c2faf80f481994c3552fe0d
PaulBoar <p.dzik1@wp.pl>
2022-06-14 12:56:37 +0200

    login in progress

--- pages/log-in.js
+++ pages/log-in.js
@@ -1,63 +1,124 @@
 import React, { useState, useEffect } from 'react';
-import { auth } from '../firebase-config.env.local';
+import { auth } from '../firebase-config.js';
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from 'firebase/auth';
 
 import styles from './log-in.module.css';
 
-function LogIn() {
+function LogIn(props) {
   const [wantRegister, setWantRegister] = useState(false);
+  const [isLogged, setIsLogged] = useState(false);
+  const [registerName, setRegisterName] = useState('');
   const [registerEmail, setRegisterEmail] = useState('');
   const [registerPassword, setRegisterPassword] = useState('');
   const [loginEmail, setLoginEmail] = useState('');
   const [loginPassword, setLoginPassword] = useState('');
-console.log(auth)
-  // const register = async () => {
-  //  try {
 
-  //  } catch (error) {
-  //   console.log(error)
-  //  }
-  // };
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+  }, []);
 
-  const login = async () => {};
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    console.log('first');
+    try {
+      const user = createUserWithEmailAndPassword(
+        auth,
+        registerEmail,
+        registerPassword,
+        registerName,
+      );
+      setIsLogged(true);
+      props.onIsLogged(true);
+      props.onLoggedName(registerName)
+      console.log(user.email);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  const logout = async () => {};
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    console.log('lol');
+  };
 
-  const logIn = !wantRegister ? (
+  const logout = async () => {
+    await signOut(auth)
+    setIsLogged(false);
+    props.onIsLogged(false);
+    props.onIsLogged(isLogged);
+  };
+
+  let logIn = (
     <>
-      <h2>Log in</h2>
-      <form>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
         <label>Email</label>
-        <input type='text' placeholder='email...' />
+        <input
+          type='text'
+          placeholder='email...'
+          value={loginEmail}
+          onChange={(e) => setLoginEmail(e.target.value)}
+        />
         <label>Password</label>
-        <input type='password' placeholder='password...' />
+        <input
+          type='password'
+          placeholder='password...'
+          value={loginPassword}
+          onChange={(e) => setLoginPassword(e.target.value)}
+        />
         <button type='submit'>Login</button>
         <button type='button' onClick={() => setWantRegister(true)}>
-          Sign up
+          Register
         </button>
-      </form>{' '}
+      </form>
     </>
-  ) : (
+  );
+
+  let register = (
     <>
-      {' '}
       <h2>Sign up</h2>
-      <form>
+      <form onSubmit={handleRegister}>
         <label>Username</label>
-        <input type='text' placeholder='name...' />
+        <input type='text' placeholder='name...' value={registerName} onChange={e => setRegisterName(e.target.value)} />
         <label>Email</label>
-        <input type='text' placeholder='email...' />
+        <input
+          type='text'
+          placeholder='email...'
+          vlaue={registerEmail}
+          onChange={(e) => setRegisterEmail(e.target.value)}
+        />
         <label>Password</label>
-        <input type='password' placeholder='password...' />
-        <button type='submit'>Sign up</button>
+        <input
+          type='password'
+          placeholder='password...'
+          value={registerPassword}
+          onChange={(e) => setRegisterPassword(e.target.value)}
+        />
+        <button type='submit'>Register</button>
         <button type='button' onClick={() => setWantRegister(false)}>
-          Log in
+          Login
         </button>
-      </form>{' '}
+      </form>
     </>
   );
 
   return (
     <div className={styles.container}>
-      <div className={styles.box}>{logIn}</div>
+      {!isLogged ? (
+        <div className={styles.box}>{wantRegister ? register : logIn}</div>
+      ) : (
+        <div>
+          WELCOME {user?.email}
+          <button onClick={logout}>logout</button>
+        </div>
+      )}
     </div>
   );
 }
