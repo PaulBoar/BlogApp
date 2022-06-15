a8717aeaa0225ab4adde957a710eb967bcbe9c0c
PaulBoar <p.dzik1@wp.pl>
2022-06-15 22:53:56 +0200

    added author of the post

--- components/comments-section/comments-section.js
+++ components/comments-section/comments-section.js
@@ -2,10 +2,10 @@ import { useState, useEffect } from 'react';
 
 import styles from './comments-section.module.css';
 
-function CommentsSection({ slug, logged }) {
+function CommentsSection({ slug, logged, user }) {
   const [comment, setComment] = useState('');
   const [comments, setComments] = useState([]);
-
+console.log(user?.user?.email)
   const handleAddComment = (e) => {
     setComment(e.target.value);
   };
@@ -13,34 +13,34 @@ function CommentsSection({ slug, logged }) {
   const addCommentHandler = (e) => {
     e.preventDefault();
 
-    postComments({ comment: comment });
+    postComments({ comment: comment, author: user?.user?.email });
     setComment('');
   };
 
+  let loadedComments = [];
   async function fetchComments() {
     const res = await fetch(
       `https://blogapp-c6647-default-rtdb.firebaseio.com/comments/${slug}.json`
     );
     const data = await res.json();
     console.log('fetchin');
-    let loadedComments = [];
 
     for (const key in data) {
-      loadedComments.push({ id: key, comment: data[key].comment });
+      loadedComments.push({ id: key, comment: data[key].comment, author: data[key].author });
     }
     setComments(loadedComments);
+    console.log(loadedComments)
   }
-  
   useEffect(() => {
     fetchComments();
   }, []);
 
-  async function postComments(comment) {
+  async function postComments(comment, user ) {
     const res = await fetch(
       `https://blogapp-c6647-default-rtdb.firebaseio.com/comments/${slug}.json`,
       {
         method: 'POST',
-        body: JSON.stringify(comment),
+        body: JSON.stringify(comment, user?.user.email),
         headers: {
           'Content-Type': 'application/json',
         },
@@ -79,7 +79,7 @@ console.log(logged)
           value={comment}
           onChange={handleAddComment}
         />
-        <button disabled={!logged} type='submit' className={styles.btn}>
+        <button disabled={!user} type='submit' className={styles.btn}>
           Add comment
         </button>
       </form>
@@ -87,8 +87,7 @@ console.log(logged)
         Forum
         {comments &&
           comments.map((com, i) => {
-            return <p>{com.comment}
-            <button onClick={i => handleRemove(i)}>delete</button></p>;
+            return <p>{com.comment} <span>{com.author}</span></p>;
           })}
       </div>
     </div>
