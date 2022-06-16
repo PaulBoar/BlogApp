3fb8d47ac53a2e88f1b84f7c43ce06802ea34cc2
PaulBoar <p.dzik1@wp.pl>
2022-06-16 21:36:11 +0200

    burger menu

--- components/layout/burger.js
+++ components/layout/burger.js
@@ -1,11 +1,15 @@
 import styles from './burger.module.css'
 
-function Burger() {
+function Burger({onShowBurgerMenu, isOpen}) {
+ const handleShowBurgerMenu = () => {
+  onShowBurgerMenu()
+ }
+
   return (
-   <button className={styles.burger}>
-   <div/>
-   <div/>
-   <div/>
+   <button className={styles.burger} onClick={handleShowBurgerMenu}>
+   <div style={isOpen ? {transform: 'rotate(45deg)'} : null}/>
+   <div style={isOpen ? {transform: 'translateX(-100%)', backgroundColor: 'transparent'} : null}/>
+   <div style={isOpen ? {transform: 'rotate(-45deg)'} : null}/>
   </button>
   )
 }
