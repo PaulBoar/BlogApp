10012f15a1fa6fbe3f3c2545e1d065dac6cc4fd6
PaulBoar <p.dzik1@wp.pl>
2022-06-16 22:07:43 +0200

    burger displaying

--- components/layout/main-nav.js
+++ components/layout/main-nav.js
@@ -1,12 +1,19 @@
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import styles from './main-nav.module.css';
+import Burger from './burger';
+import BurgerMenu from './burger-menu';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 function MainNav(props) {
   const [isLogged, setIsLogged] = useState(false);
+  const [showBurgerMenu, setShowBurgerMenu] = useState(false);
+
+  // useEffect(() => {
+  //   setShowBurgerMenu(false)
+  // }, [])
 
   useEffect(() => {
     if (props.user) {
@@ -18,12 +25,23 @@ function MainNav(props) {
     props.onLogout();
   };
 
+  const handleBurgerMenu = () => {
+    console.log('lewp')
+    setShowBurgerMenu(prev => !prev)
+  }
+
+  const handleClose = () => {
+    console.log('lewp')
+    setShowBurgerMenu(false)
+  }
+
+ 
   return (
     <header className={styles.header}>
       <Link href='/'>
         <a className={styles.logo}>
           <div className={styles.logo}>
-            Blog<span>Life</span>
+            Mind<span>works</span>
           </div>
         </a>
       </Link>
@@ -42,7 +60,8 @@ function MainNav(props) {
             )}
           </li>
         </ul>
-        <FontAwesomeIcon className={styles.burger} icon={faBars} />
+        <Burger className={styles.burger} onShowBurgerMenu={handleBurgerMenu}isOpen={showBurgerMenu}/>
+        <BurgerMenu isOpen={showBurgerMenu} logged={props.logged} onClose={handleClose}/>
       </nav>
     </header>
   );
