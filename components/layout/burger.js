68a834f2e479bfbddecd6279757dcab77431a26d
PaulBoar <p.dzik1@wp.pl>
2022-06-16 18:42:37 +0200

    burger menu

---
+++ components/layout/burger.js
@@ -0,0 +1,13 @@
+import styles from './burger.module.css'
+
+function Burger() {
+  return (
+   <button className={styles.burger}>
+   <div/>
+   <div/>
+   <div/>
+  </button>
+  )
+}
+
+export default Burger
