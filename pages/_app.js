00c7b5030443511cdda47741e55dfd305c509195
PaulBoar <p.dzik1@wp.pl>
2022-06-17 11:40:23 +0200

    loading spinner

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,33 @@
 import '../styles/globals.css';
 import Layout from '../components/layout/layout';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+function Loading() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const handleStart = (url) => (url !== router.asPath) && setLoading(true);
+    const handleComplete = () => setLoading(false);
+     
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleComplete);
+    };
+  });
+  return (
+    loading && (
+      <div className='spinner-wrapper'>
+        <div className='spinner'></div>
+      </div>
+    )
+  );
+}
 
 function MyApp({ Component, pageProps }) {
   const [isLog, setIsLog] = useState(false);
@@ -8,11 +35,11 @@ function MyApp({ Component, pageProps }) {
 
   const handleIsLogged = (props) => {
     setIsLog(props);
-  }
+  };
 
   const handleUser = (props) => {
-    setUser(props)
-  }
+    setUser(props);
+  };
 
   const handleLogout = () => {
     setIsLog(false);
@@ -20,7 +47,7 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <Layout isLogged={isLog} onLogout={handleLogout} user={user}>
-      <Component
+      <Loading/><Component
         {...pageProps}
         onIsLogged={handleIsLogged}
         onUser={handleUser}
