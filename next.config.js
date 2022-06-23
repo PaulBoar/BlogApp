da2734355d723a1ec14f9bec82d95794f7e6043f
PaulBoar <p.dzik1@wp.pl>
2022-06-23 19:13:25 +0200

    CSP

--- next.config.js
+++ next.config.js
@@ -7,13 +7,13 @@ const nextConfig = {
   },
   headers: {
     source: '/',
-    // headers: [
-    //   {
-    //     key: 'Content-Security-Policy',
-    //     value:
-    //       "default-src 'self' https: ; script-src 'self' ; object-src 'none'",
-    //   },
-    // ],
+    headers: [
+      {
+        key: 'Content-Security-Policy',
+        value:
+          "default-src 'self' https: ; script-src 'self' ; object-src 'none'",
+      },
+    ],
   },
   basePath: '/BlogApp',
   assetPrefix: '/BlogApp',
