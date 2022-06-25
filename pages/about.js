df3c28104e97e3f9ad07fca5d0a483aed7d0dfe7
PaulBoar <p.dzik1@wp.pl>
2022-06-25 08:58:54 +0200

    about

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,17 @@
 import styles from './about.module.css'
+import Image from 'next/iamge'
 
 function About() {
+
+  const contentfulLoader = ({ src, quality, width }) => {
+    const params = [`w=${width}`];
+  
+    if (quality) {
+      params.push(`q=${quality}`);
+    }
+    return `${src}?${params.join('&')}`;
+  };
+
   return (
    
     <div className={styles.about}>
@@ -9,7 +20,15 @@ function About() {
       <h2>About Mindworks</h2>
       <p>We are team of journalists writting about scientific methods which can improve your performance and general well-being.</p>
       </div>
-      <div className={styles.image}></div>
+      <div className={styles.image}>
+        <Image
+        src={'../public/imgs/aboutpng.png'}
+        loader={contentfulLoader}
+        alt={'working crew'}
+        width={400}
+        height={215}
+        layout='responsive' />
+      </div>
     </div>
     </div>
   )
