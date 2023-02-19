2783f3d8a54afcc5b44bd5bde9a06b36ad9b6019
Paweł Dzik <p.dzik1@wp.pl>
2023-02-19 21:40:49 +0100

    nextjs config

--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@
 
 const nextConfig = {
   reactStrictMode: true,
-  env:{
-    NEXT_PUBLIC_CONTENTFUL_SPACE_ID : process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-    NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY,
-  },
+  // env:{
+  //   NEXT_PUBLIC_CONTENTFUL_SPACE_ID : process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+  //   NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY,
+  // },
   images: {
     loader: 'akamai',
     path: '',
