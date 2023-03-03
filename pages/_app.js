01e9eddbd4a57628e380dbf511a814fc7e2d76de
Paweł Dzik <p.dzik1@wp.pl>
2023-03-03 16:53:43 +0100

    title and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -2,61 +2,67 @@ import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
+import Head from 'next/head';
 
 function Loading() {
-  const router = useRouter();
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    const handleStart = (url) => url !== router.asPath && setLoading(true);
-    const handleComplete = () => setLoading(false);
+	const router = useRouter();
+	const [loading, setLoading] = useState(false);
+	useEffect(() => {
+		const handleStart = (url) => url !== router.asPath && setLoading(true);
+		const handleComplete = () => setLoading(false);
 
-    router.events.on('routeChangeStart', handleStart);
-    router.events.on('routeChangeComplete', handleComplete);
-    router.events.on('routeChangeError', handleComplete);
+		router.events.on('routeChangeStart', handleStart);
+		router.events.on('routeChangeComplete', handleComplete);
+		router.events.on('routeChangeError', handleComplete);
 
-    return () => {
-      router.events.off('routeChangeStart', handleStart);
-      router.events.off('routeChangeComplete', handleComplete);
-      router.events.off('routeChangeError', handleComplete);
-    };
-  });
-  return (
-    loading && (
-      <div className='spinner-wrapper'>
-        <div className='spinner'></div>
-      </div>
-    )
-  );
+		return () => {
+			router.events.off('routeChangeStart', handleStart);
+			router.events.off('routeChangeComplete', handleComplete);
+			router.events.off('routeChangeError', handleComplete);
+		};
+	});
+	return (
+		loading && (
+			<div className='spinner-wrapper'>
+				<div className='spinner'></div>
+			</div>
+		)
+	);
 }
 
 function MyApp({ Component, pageProps }) {
-  const [isLog, setIsLog] = useState(false);
-  const [user, setUser] = useState('');
+	const [isLog, setIsLog] = useState(false);
+	const [user, setUser] = useState('');
 
-  const handleIsLogged = (props) => {
-    setIsLog(props);
-  };
+	const handleIsLogged = (props) => {
+		setIsLog(props);
+	};
 
-  const handleUser = (props) => {
-    setUser(props);
-  };
+	const handleUser = (props) => {
+		setUser(props);
+	};
 
-  const handleLogout = () => {
-    setIsLog(false);
-  };
+	const handleLogout = () => {
+		setIsLog(false);
+	};
 
-  return (
-      <Layout isLogged={isLog} onLogout={handleLogout} user={user}>
-        <Loading />
-        <Component
-          {...pageProps}
-          onIsLogged={handleIsLogged}
-          onUser={handleUser}
-          isLogged={isLog}
-          user={user}
-        />
-      </Layout>
-  );
+	return (
+		<Layout isLogged={isLog} onLogout={handleLogout} user={user}>
+			<Head>
+				<title>Mind Works</title>
+				<meta name='description' content='Blog made in next js' />
+				<link rel='icon' href='favicon/favicon.ico' />
+			</Head>
+			<Loading />
+			<Component
+				{...pageProps}
+				onIsLogged={handleIsLogged}
+				onUser={handleUser}
+				isLogged={isLog}
+				user={user}
+			/>
+		</Layout>
+	);
 }
 
 export default MyApp;
